4e430bccefa9de5d2616cbaae80a1c47d45ca600
haejung98 <haejung98@naver.com>
2022-11-14 17:55:17 +0900

    7. 틱택토 this.$set

---
+++ 7. 틱택토/TdComponent.vue
@@ -0,0 +1,74 @@
+<template>
+    <td @click="onClickTd">{{cellData}}</td>
+</template>
+
+<script>
+
+  export default {
+    props: {
+        cellData: String,
+        rowIndex: Number,
+        cellIndex: Number,
+    },
+    components: {
+
+    },
+    data() {
+      return {
+
+      };
+    },
+    computed: {
+      
+    },
+    methods: {
+        onClickTd() {
+            // 자식컴포넌트에서 부모 컴포넌트 데이터를 조작할 수 있음
+            console.log(this.$root.$data); // 가장 최상위 부모(TicTacToe.vue) 데이터 한방에 접근할 수 있음
+            console.log(this.$parent.$data); // 부모 컴포넌트 데이터에 접근
+
+            const rootData = this.$root.$data;
+
+            // [가로행수][세로행수]에 this.$root.$data.turn을 넣어줌
+            //rootData.tableData[this.rowIndex][this.cellIndex] = rootData.turn;  // 배열의 값을 인덱스로 바꾸면 화면에 반영이 안된다.
+            this.$set(rootData.tableData[this.rowIndex], this.cellIndex, rootData.turn); // 해결방법 : 배열에서 index 쓸 땐 this.$set을 이용
+
+            let win = false;
+            if (rootData.tableData[this.rowIndex][0] === rootData.turn && rootData.tableData[this.rowIndex][1] === rootData.turn && rootData.tableData[this.rowIndex][2] === rootData.turn) {
+                win = true; // 가로 세줄
+            }
+            if (rootData.tableData[0][this.cellIndex] === rootData.turn && rootData.tableData[1][this.cellIndex] === rootData.turn && rootData.tableData[2][this.cellIndex] === rootData.turn) {
+                win = true; // 세로 세줄
+            }
+            if (rootData.tableData[0][0] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][2] === rootData.turn) {
+                win = true; // 대각선
+            }
+            if (rootData.tableData[0][2] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][0] === rootData.turn) {
+                win = true; // 대각선
+            }
+            if (win) { // 이긴 경우: 3줄 달성
+                rootData.winner = rootData.turn; // 승자기록
+                // 초기화
+                rootData.turn = 'O';
+                rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
+            } else { // 무승부
+                let all = true; // all이 true면 무승부라는 뜻
+                rootData.tableData.forEach((row) => { // 무승부 검사
+                    row.forEach((cell) => {
+                        if (!cell) {
+                            all = false;
+                        }
+                    });
+                });
+                if (all) { // 무승부
+                    rootData.winner = '';
+                    rootData.turn = 'O';
+                    rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
+                } else {    // 게임이 안끝났을 경우엔 turn을 다음사람 turn으로
+                    rootData.turn = rootData.turn === 'O' ? 'X' : 'O';
+                }
+            }
+        }
+    }
+  };
+</script>
---
+++ 7. 틱택토/TicTacToe.vue
@@ -0,0 +1,58 @@
+<!-- 최상위 부모 컴포넌트 -->
+<template>
+  <div>
+    <div>{{turn}}님의 턴입니다.</div>
+    <table-component :table-data="tableData" />
+    <div v-if="winner">{{winner}}님의 승리!</div>
+  </div>
+</template>
+
+<script>
+  import TableComponent from './TableComponent';
+  export default {
+    components: {
+      TableComponent,
+    },
+    data() {
+      return {
+        tableData: [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', ''],
+        ],
+        turn: 'O', // o팀, x팀 차례
+        winner: '',
+      };
+    },
+    computed: {
+      
+    },
+    methods: {
+      onChangeData() {
+        // this.tableData[1][0] = 'X'; 작동하지 않음
+        this.$set(this.tableData[1], 0, 'X'); // Vue.set과 동일
+      }
+    },
+    mounted() {
+
+    },
+    beforeDestroy() {
+
+    },
+    watch: {
+
+    },
+  };
+</script>
+
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+  }
+</style>
